d008dabb408e761de56023a26d82ac1e02a74d61
App Academy Student <student@appacademy.io>
2017-01-04 13:55:40 -0800

    Phase 2: Pokemon Redux Cycle - added Thunk middleware

--- pokemon/frontend/pokedex.jsx
+++ pokemon/frontend/pokedex.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import configureStore from './store/store';
 import { fetchAllPokemon } from './util/api_util';
-import { receiveAllPokemon } from './actions/pokemon_actions';
+import { receiveAllPokemon, requestAllPokemon } from './actions/pokemon_actions';
 
 document.addEventListener('DOMContentLoaded', () => {
   const rootEl = document.getElementById('root');
@@ -10,5 +10,6 @@ document.addEventListener('DOMContentLoaded', () => {
   window.store = store;
   window.fetchAllPokemon = fetchAllPokemon;
   window.receiveAllPokemon = receiveAllPokemon;
+  window.requestAllPokemon = requestAllPokemon;
   ReactDOM.render(<h1>Pokedex</h1>, rootEl);
 });
