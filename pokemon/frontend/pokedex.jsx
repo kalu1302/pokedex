4c11fd3256ba30e1014a5d4b7636d30fb32e4436
App Academy Student <student@appacademy.io>
2017-01-04 13:42:18 -0800

    Phase 2: Pokemon Redux Cycle - pokemonReducer, rootReducer, and store

--- pokemon/frontend/pokedex.jsx
+++ pokemon/frontend/pokedex.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import configureStore from './store/store';
 import { fetchAllPokemon } from './util/api_util';
 import { receiveAllPokemon } from './actions/pokemon_actions';
 
 document.addEventListener('DOMContentLoaded', () => {
   const rootEl = document.getElementById('root');
+  const store = configureStore();
+  window.store = store;
   window.fetchAllPokemon = fetchAllPokemon;
   window.receiveAllPokemon = receiveAllPokemon;
   ReactDOM.render(<h1>Pokedex</h1>, rootEl);
